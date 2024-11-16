445a76c9e7a39028275d66626bf7ee439553a173
Prabin Bohara <prabinbohara10@gmail.com>
2024-11-17 00:23:47 +0545

    feat: implemented GazeGloudAPI to the pipeline and trained model

--- src/models.py
+++ src/models.py
@@ -4,7 +4,7 @@ import torch
 
 ## LSTM Model
 class GazeLSTM(nn.Module):
-    def __init__(self, input_size=2, hidden_size=64, num_layers=2, num_classes=2):
+    def __init__(self, input_size=7, hidden_size=64, num_layers=2, num_classes=2):
         super(GazeLSTM, self).__init__()
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, num_classes)
@@ -18,7 +18,7 @@ class GazeLSTM(nn.Module):
     
 # Model for generating embeddings
 class GazeEmbeddingModel(nn.Module):
-    def __init__(self, input_size=2, hidden_size=64, embedding_size=128, num_layers=2):
+    def __init__(self, input_size=7, hidden_size=64, embedding_size=128, num_layers=2):
         super(GazeEmbeddingModel, self).__init__()
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, embedding_size)
